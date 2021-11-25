a79835c4a855d7f19e5ca4f23a9c175a047739df
Shokochoco <shsuzu1992@gmail.com>
2021-11-25 10:57:05 +0900

    Before review

--- Kadai-5/ViewController.swift
+++ Kadai-5/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  Kadai-5
-//
-//  Created by 鈴木淳子 on 2021/11/20.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    @IBOutlet private weak var countTextField1: UITextField!
+    @IBOutlet private weak var countTextField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction private func calculateButtonTapped(_ sender: Any) {
+        guard let num1 = Double(countTextField1.text!) else {
+          alert(message: "割られる数を入力して下さい")
+         return
+        }
+
+        guard let num2 = Double(countTextField2.text!) else {
+            alert(message: "割る数を入力して下さい")
+            return
+        }
+
+        guard num2 != 0 else { // num2 is 0
+            alert(message: "割る数に0を入力しないでください")
+            return
+        }
+          let calculated = num1 / num2
+          resultLabel.text = String(calculated)
     }
 
-
+    private func alert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        let okAlert = UIAlertAction(title: "OK", style: .default) { _ in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(okAlert)
+        present(alert, animated: true, completion: nil)
+    }
 }
-
